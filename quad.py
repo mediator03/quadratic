a6f83b7bb64233565a2134b03475c0d63c0c3b1d
mediator03 <zhenghui01@gmail.com>
2016-09-12 13:25:36 -0400

    Update quad.py

--- quad.py
+++ quad.py
@@ -19,3 +19,17 @@ while True:
     finally: #if no error occur, print solutions. 
         print (x1,x2)
 
+
+# alternate method, use if instead of try
+import math
+import sys
+a = float(input("Please enter a"))
+b = float(input("Please enter b"))
+c = float(input("Please enter c"))
+if b**2-4*a*c < 0:
+    print ("error, can't solve negative square root")
+    sys.exit()
+else:
+    x1 = (-b+math.sqrt(b**2-4*a*c))/(2*a)
+    x2 = (-b-math.sqrt(b**2-4*a*c))/(2*a)
+print (x1,x2)
